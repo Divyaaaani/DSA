49bb388e8d0e73bd5b5776e4bccb0b7f2c4c0a47
Divyaaaani <divyanikhandait1234@gmail.com>
2026-01-07 20:54:58 +0530

    second day

--- Armstrong.java
+++ Armstrong.java
@@ -1,21 +1,38 @@
-class Armstrong {
-    public static boolean Armstrong(int x) {
-      
+// class Armstrong {
+//     public static boolean Armstrong(int x) {
+//         int sum = 0, rem;
+//         int y = x;
+//         while (x > 0) {
+//             rem = x % 10;
+//             sum= sum + (rem*rem*rem);
+//             x = x / 10;
+//         }
+//         if (sum == y)
+//         return true ;
+//         else 
+//         return false;
+//     }
 
-        int sum = 0, rem;
-        int y = x;
-        while (x > 0) {
-            rem = x % 10;
-            sum= sum + (rem*rem*rem);
-            x = x / 10;
+//     public static void main(String[] args) {
+//         System.out.println(Armstrong(153)); // call directly
+//     }
+// }
+import java.lang.*;
+class Armstrong{
+    public static boolean isArm(int n){
+        int m=n;
+        int arm=0;
+        while(n>0){
+            int rem=n%10;
+            arm = arm+ (rem*rem*rem);
+            n=n/10;
         }
-        if (sum == y)
-        return true ;
-        else 
+        if (arm==m){
+            return true;
+        }else
         return false;
     }
-
-    public static void main(String[] args) {
-        System.out.println(Armstrong(153)); // call directly
+    public static void main(String[] args){
+        System.out.println(isArm(3442));
     }
 }
--- isPalindrome.java
+++ isPalindrome.java
@@ -1,22 +1,40 @@
-//returns boolean 
-class isPalindrome {
-    public static boolean isPalindrome(int x) {
-        if (x < 0) 
-        return false;
+// //returns boolean 
+// class isPalindrome {
+//     public static boolean isPalindrome(int x) {
+//         if (x < 0) 
+//         return false;
 
-        else{
-            int rev = 0, rem;
-        int y = x;
-        while (x > 0) {
-            rem = x % 10;
-            rev = rev * 10 + rem;
-            x = x / 10;
+//         else{
+//             int rev = 0, rem;
+//         int y = x;
+//         while (x > 0) {
+//             rem = x % 10;
+//             rev = rev * 10 + rem;
+//             x = x / 10;
+//         }
+//         return rev == y;
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         System.out.println(isPalindrome(1221)); // call directly
+//     }
+// }
+
+
+import java.lang.*;
+class isPalindrome{
+    static boolean isPalindrome(int n){
+        int m =n;
+        int rev=0;
+        while(n>0){
+        int lastdigit= n%10;
+        rev= rev * 10 + lastdigit;
+        n=n/10;
         }
-        return rev == y;
-        }
+        return rev==m;
     }
-
-    public static void main(String[] args) {
-        System.out.println(isPalindrome(1221)); // call directly
+    public static void main(String[] args){
+        System.out.println(isPalindrome(1261));
     }
 }
